589b99951443bfd22e326e33fd142e81eae8a7de
NavjotKaur31 <kau21372@sheridancollege.ca>
2026-09-23 09:46:18 -0400

    Update comments in StudentList.java
    Updated comments in StudentList.java to reflect today's date and purpose.

--- src/main/java/com/mycompany/hellof46/StudentList.java
+++ src/main/java/com/mycompany/hellof46/StudentList.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  Today is wednesday sept
+ to show pull
  */
 package com.mycompany.hellof46;
 
