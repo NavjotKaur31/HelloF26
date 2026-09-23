b49db2bf5674fa1b15fa39358e73b15fd0866cac
Navjot Kaur <kau21372@sheridancollege.ca>
2026-09-23 10:42:21 -0400

    added a new class

---
+++ src/main/java/fulltimestudent.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import com.mycompany.hellof46.Student;
+
+/**
+ *
+ * @author DELL
+ */
+public class fulltimestudent extends Student {
+    public fulltimestudent(String sID.String sName){
+        super(sID,sName);
+    }
+}
